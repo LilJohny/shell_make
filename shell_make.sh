ca4f264022c810782242eb62350a2b240b8cf796
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-09-17 21:40:21 +0300

    a

--- shell_make.sh
+++ shell_make.sh
@@ -1,16 +1,45 @@
 #!/usr/bin/env bash
 
+generate_targets_need_run(){
+  target="${1}"
+  if ! [[ " ${run_targets[@]} " =~ " ${target} " ]]; then
+    run_targets+=("$target")
+  fi
+  dependencies=${targets["$target"]}
+  export IFS=" "
+  for dependecy in $dependencies; do
+
+    if ! [[ $dependecy == "" ]] && ! [[ " ${run_targets[@]} " =~ " ${dependecy} " ]]; then
+      run_targets+=("$dependecy")
+      generate_targets_need_run "$dependecy"
+    fi
+  done
+}
+
+execute_target_command(){
+  target=$1
+  eval "${recipes[$target]}"
+}
+
+
 makefile="shmakefile"
-echo start
+next_arg_file=0
+targets=[]
+run_targets=()
+given_args=()
+
 for var in "$@"
 do
-    echo "$var"
+    if [[ $var == "-f" ]]; then
+      next_arg_file=1
+    elif [[ $next_arg_file == 1 ]]; then
+      makefile="$var"
+      next_arg_file=0
+    else
+      given_args+=("$var")
+    fi
 done
-echo finish
 
-if [ "$1" != "" ]; then
-  makefile="$1"
-fi
 
 declare -A targets
 declare -A recipes
@@ -26,10 +55,8 @@ while IFS= read -r line; do
     if [[ $line == *: ]]; then
       targets["$l_target"]=""
     else
-      echo 23
       dep="$(cut -d':' -f 2 <<<"$line")"
       dep="$(echo "$dep" |   xargs)"
-      echo $dep
       targets["$l_target"]=$dep
     fi
 
@@ -38,8 +65,10 @@ while IFS= read -r line; do
     if ! [[ $line =~ ^(\s+)?$ ]] && ! [[ $l_target == "" ]]; then
 
       if ! [[ $current_command == "" ]]; then
+        line="$(echo "$line" | xargs)"
         current_command="$current_command && $line"
       else
+        line="$(echo "$line" | xargs)"
         current_command=$line
       fi
 
@@ -48,15 +77,19 @@ while IFS= read -r line; do
     fi
   fi
 done <"$makefile"
+if [[ "${#given_args[@]}" == 0 ]]; then
+    generate_targets_need_run "${targets[0]}"
+else
+    for var in "${given_args[@]}"
+    do
+      generate_targets_need_run "$var"
+    done
+fi
 
-for i in "${!recipes[@]}"; do
-  echo "key  : $i"
-  echo "value: ${recipes[$i]}"
+
+
+for (( i=${#run_targets[@]}-1 ; i>=0 ; i-- )) ;
+do
+    target="${run_targets[i]}"
+    execute_target_command "$target"
 done
-
-echo recipes here
-for i in "${!targets[@]}"; do
-  echo "key  : $i"
-  echo "value: ${targets[$i]}"
-done
-
